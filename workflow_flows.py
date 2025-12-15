42c9e5405577aaa60cc71d0c89802f636b588813
KAYALAIMMANUELRAJU <immanuelrajkayala@gmail.com>
2025-12-15 16:43:16 +0530

    Update workflow_flows.py

--- workflow_flows.py
+++ workflow_flows.py
@@ -12,11 +12,17 @@ def transform(data):
 def load(data):
     return f"{data}"  # Logs will not show in Cloud
 
+@task
+def validate(data):
+    return f"{data} → Validated"
+
+
 @flow
 def etl_flow(job_name: str = "Daily ETL"):
-    raw = extract()
     processed = transform(raw)
-    load(f"{job_name}: {processed}")
+    validated = validate(processed)
+    load(validated)
+
 
 @flow
 def post_etl_flow():
